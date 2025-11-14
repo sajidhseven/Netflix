266695aa62f87286b099195d153f4e2aebc6d4fc
sajidhseven <sajidhseven@gmail.com>
2025-11-14 11:06:46 +0530

    Update profile images to new Pinterest links

--- port/src/components/ProfileSelection.jsx
+++ port/src/components/ProfileSelection.jsx
@@ -10,19 +10,19 @@ const ProfileSelection = () => {
     {
       id: 1,
       name: 'Recruiter',
-      image: 'https://user-gen-media-assets.s3.amazonaws.com/seedream_images/f8bfb35d-17a3-4460-8211-7febc70c92b3.png',
+      image: 'https://www.pinterest.com/pin/639018634670352062/',
       route: '/home'
     },
     {
       id: 2,
       name: 'Developer',
-      image: 'https://user-gen-media-assets.s3.amazonaws.com/seedream_images/625c2a91-257e-4092-a247-af04dbc0fdcf.png',
+      image: 'https://www.pinterest.com/pin/214484000998761471/',
       route: '/home'
     },
     {
       id: 3,
       name: 'Stalker',
-      image: 'https://user-gen-media-assets.s3.amazonaws.com/seedream_images/89e74f18-69c9-48e1-9c34-1dd2dfe6e495.png',
+      image: 'https://www.pinterest.com/pin/375909900167651520/',
       route: '/home'
     }
   ];
@@ -65,3 +65,4 @@ const ProfileSelection = () => {
 };
 
 export default ProfileSelection;
+
